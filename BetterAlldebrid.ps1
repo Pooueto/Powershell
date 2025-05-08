e8a5b5d797bbc9fe4ca6deca3b91ebd02649980a
Chagrin Théo <56735136+Pooueto@users.noreply.github.com>
2025-05-08 22:23:12 +0200

    Add files via upload

--- BetterAlldebrid.ps1
+++ BetterAlldebrid.ps1
@@ -1,5 +1,5 @@
 # ========= AUTO-MISE À JOUR =========
-$LocalVersion = "2.0.0"
+$LocalVersion = "2.1.0"
 
 $RemoteScriptUrl = "https://raw.githubusercontent.com/Pooueto/Powershell/refs/heads/main/BetterAlldebrid.ps1"
 
